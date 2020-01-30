4ee5240112cd49d510bd5c3c5e1e84abeb22ea43
andryushque <andrew.ustinov23@gmail.com>
2020-01-30 22:11:11 +0300

    add owlCarousel - responsive

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
 $(function() {
 
+    /*=== mmenu & hamburger ===*/
     $('#my-menu').mmenu({
         extensions: [ "theme-black", "effect-menu-slide", "pagedim-black"],
         navbar: {
@@ -16,6 +17,27 @@ $(function() {
         $('.hamburger').addClass('is-active');
     }).bind('closed', function() {
         $('.hamburger').removeClass('is-active');
+    });
+
+
+    /*=== OwlCarousel - services ===*/
+    $(".carousel-services").owlCarousel({
+        loop: true,
+        nav: true,
+        smartSpeed: 700,
+        navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
     });
 
 });
